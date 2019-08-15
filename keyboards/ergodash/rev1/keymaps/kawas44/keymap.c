faf53677c7c3c3a3f3fd86fa1d2352ec0a6ba9c7
kawas44 <kawas44@gmail.com>
2019-08-16 00:44:20 +0200

    remove dead code

--- keyboards/ergodash/rev1/keymaps/kawas44/keymap.c
+++ keyboards/ergodash/rev1/keymaps/kawas44/keymap.c
@@ -1,18 +1,9 @@
 #include QMK_KEYBOARD_H
 
-extern keymap_config_t keymap_config;
-
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-};
 
 #define BSPC_L1 LT(_LOWER, KC_BSPC)
 #define CTL_OSM OSM(MOD_LCTL)
@@ -23,6 +14,7 @@ enum custom_keycodes {
 #define SPC_L2  LT(_RAISE, KC_SPC)
 
 #define ALT_ESC LALT_T(KC_ESC)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -66,73 +58,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          BSPC_L1, CTL_OSM, DEL_GUI,     ENT_ALT, SFT_OSM, SPC_L2          , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
 // ,------------------------------------------------------------------------|  |------------------------------------------------------------------------.
-  ),
-
-  [_ADJUST] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
-    _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                       _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-    _______, _______, BL_TOGG, BL_BRTG, BL_INC , BL_DEC ,_______,                       _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______  \
   )
 };
 
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-#endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
 #ifdef CONSOLE_ENABLE
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
   debug_enable=true;
   /* debug_matrix=true; */
   debug_keyboard=true;
-  //debug_mouse=true;
+  /* debug_mouse=true; */
 }
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-         print("mode just switched to qwerty and this is a huge string\n");
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
-  }
   return true;
 }
