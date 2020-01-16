cef84acd2d078b21138bdf5a6eeab37fd6f4c0ad
kawas44 <kawas44@gmail.com>
2020-01-17 00:32:57 +0100

    ergodash: azerty typematrix like

--- keyboards/ergodash/rev1/keymaps/kawas44/keymap.c
+++ keyboards/ergodash/rev1/keymaps/kawas44/keymap.c
@@ -32,13 +32,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_AZERTY] = LAYOUT(
 // ,-------+--------+--------+--------+--------+--------+--------.                     ,--------+--------+--------+--------+--------+--------+--------.
-    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_LBRC,                       KC_RBRC, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
+    FR_SUP2, FR_AMP , FR_EACU, FR_QUOT, FR_APOS, FR_LPRN, FR_MINS,                       FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV, FR_RPRN, FR_EQL , FR_DLR ,
 // |-------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_DEL ,                       KC_ENT , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_EQL ,
+    KC_TAB , FR_A   , FR_Z   , KC_E   , KC_R   , KC_T   , KC_BSPC,                       KC_BSPC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , FR_CIRC,
 // |-------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
-    KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_NO  ,                       KC_NUHS, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    KC_ESC , FR_Q   , KC_S   , KC_D   , KC_F   , KC_G   , KC_ENT ,                       KC_ENT , KC_H   , KC_J   , KC_K   , KC_L   , FR_M   , FR_UGRV,
 // |-------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
-    KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX,                       XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_NO  ,
+    FR_LESS, FR_W   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX,                       XXXXXXX, KC_N   , FR_COMM, FR_SCLN, FR_COLN, FR_EXLM, FR_ASTR,
 // |-------+--------+--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------+--------+--------|
     KC_NO  , KC_LCTL, KC_LGUI, KC_LALT,          MO_L1  , KC_BSPC, KC_LSFT,     KC_RSFT, KC_SPC , MO_L2  ,          KC_RALT, KC_RGUI, KC_RCTL, KC_NO
 // `-------+--------+--------+--------+        `--------+--------+--------'   `--------+--------+--------'        `--------+--------+--------+--------'
@@ -62,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ,-------+--------+--------+--------+--------+--------+--------.                     ,--------+--------+--------+--------+--------+--------+--------.
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // |-------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
-    KC_PSCR, KC_F12 , KC_F11 , KC_F10 , KC_F9  , KC_VOLU, XXXXXXX,                       XXXXXXX, KC_PGUP, KC_HOME, KC_UP  , KC_END , XXXXXXX, XXXXXXX,
+    KC_PSCR, KC_F12 , KC_F11 , KC_F10 , KC_F9  , KC_VOLU, XXXXXXX,                       XXXXXXX, KC_INS , KC_DEL , KC_ESC , XXXXXXX, XXXXXXX, XXXXXXX,
 // |-------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
-    XXXXXXX, KC_F8  , KC_F7  , KC_F6  , KC_F5  , KC_MPLY, XXXXXXX,                       XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F8  , KC_F7  , KC_F6  , KC_F5  , KC_MPLY, XXXXXXX,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
 // |-------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
-    XXXXXXX, KC_F4  , KC_F3  , KC_F2  , KC_F1  , KC_VOLD, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F4  , KC_F3  , KC_F2  , KC_F1  , KC_VOLD, XXXXXXX,                       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_ENT , XXXXXXX, XXXXXXX,
 // |-------+--------+--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------+--------+--------|
     XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, _______, _______,     _______, _______, _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX
 // `-------+--------+--------+--------+        `--------+--------+--------'   `--------+--------+--------'        `--------+--------+--------+--------'
